5ca022203d1b4a0468b640df46fc66966dbfcad5
Jared Lewis <jared@jared.kiwi.nz>
2018-12-09 19:48:09 +1100

    Added required packages to setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     version="0.1.1",
     author="Jared Lewis",
     author_email="jared@bodekerscientific.com",
-    description="A basic simple climate model in Python.",
-    license="BSD",
+    description="A basic simple climate model in Python",
+    license="MIT",
     keywords="simple climate model SCM climate science",
     url="http://pythonhosted.org/pySCM",
     packages=['pySCM'],
@@ -25,7 +25,27 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: BSD License",
-        "Intended Audience :: Science/Research"
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=[
+        "matplotlib",
+        "numpy"
+    ],
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/bodekerscientific/pyscm/issues",
+        "Source": "https://github.com/bodekerscientific/pyscm/",
+    },
+    extras_require={
+        "docs": ["sphinx >= 1.4", "sphinx_rtd_theme", "sphinx-autodoc-typehints"],
+        "tests": ["pytest", "pytest-cov", "codecov", "nbval", "notebook"],
+        "dev": [
+            "setuptools>=38.6.0",
+            "twine>=1.11.0",
+            "wheel>=0.31.0",
+        ],
+    },
 )
